c6d62586f06e1c0fef84e7e405cab845806024d4
abduljelil <abduljaleel.wees@gmail.com>
2022-08-24 19:46:51 +0300

    Home Page Topbar layout

---
+++ src/components/layout/topbar/V1.vue
@@ -0,0 +1,26 @@
+<script setup></script>
+<template>
+    <header class=" col-start-1 col-span-full bg-blue-600 grid grid-cols-12 px-3">
+        <div class="col-start-1 col-span-3 flex-col ">
+            <div class="flex items-center justify-center">logo</div>
+            <div class="flex items-center justify-center">brand name</div>
+        </div>
+        <div class="col-start-4 col-span-9 flex-col items-center justify-center">
+            <div class="flex items-center justify-center gap-4">
+                <div>link1</div>
+                <div>link1</div>
+                <div>link1</div>
+                <div>link1</div>
+                <div>link1</div>
+            </div>
+            <div class=" grid grid-cols-12">
+                <div class="col-start-1 col-span-6">
+                    <div class="flex items-center justify-center">search</div>
+                </div>
+                <div class="col-start-7 col-span-12">
+                    <div class="flex items-center justify-center">login</div>
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,13 @@
-<script setup></script>
-
+<script setup>
+  import Topbar from "@/components/layout/topbar/V1.vue";
+</script>
 <template>
-  <dev class="w-full grid grid-cols-12 ">Welcome</dev>
+  <dev class="w-full grid grid-cols-12 ">
+    <Topbar/>
+  <main class="col-start-1 col-span-12 bg-green-500 grid grid-cols-12 h-screen">main</main>
+  </dev>
+
+      
+    
+  
 </template>
